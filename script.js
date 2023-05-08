bdd41907269edd1752d777c2b0472bee2a164428
UladzimirKurchych <kurchich.v@gmail.com>
2023-05-08 10:16:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
 
 
-let num = 10;
-let sum;
 
-if (num >= 10 && num <=99) {
-	sum = num[0] + num[1];
-	if (sum <= 9) {
-		console.log('сумма цифр однозначна');
-	} else {
-		console.log('сумма цифр двузначна');
-	}
-	
-}
