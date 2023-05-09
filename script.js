0be60381f423dc07e6b2a1c75b3b5f7c91885c54
UladzimirKurchych <kurchich.v@gmail.com>
2023-05-09 10:16:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 
 
-let num = 80;
-let sum;
 
-if (num >= 10 && num <= 99) {
-	sum = +String(num)[0] + +String(num)[1];
-		if (sum >=0 && sum <= 9) {
-			console.log('qwerty');
-		} else {
-			console.log('asdf');
-		}
-}
