beef4cc9ae49706a3f7e6b8ec762e8b69da8c0be
UladzimirKurchych <kurchich.v@gmail.com>
2023-05-09 10:07:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 
 
+let num = 14;
+let sum;
+
+if (num >= 10 && num <= 99) {
+	sum = String(num)[0] + String(num)[1];
+		if (sum <= 10) {
+			console.log('сумма циф числа однозначна')
+		} else {
+			console.log('сумма цифр числа двузначна');
+		}
+}
 
