4d69879386a84840b04c0083b2aeda0d6315ffa2
UladzimirKurchych <kurchich.v@gmail.com>
2023-05-09 10:24:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 
-let lang = 'de';
 
-switch (lang) {
-	case 'ru':
-		console.log('RU');
-		break;
-	case 'en':
-		console.log('EN');
-		break;
-	case 'fr':
-		console.log('FR');
-		break;
-	default:
-		console.log('ERROR');
-}
 
