45c9c0c77b203b3eb9480520c44c3866337c8076
UladzimirKurchych <kurchich.v@gmail.com>
2023-06-02 08:31:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+let arr = [1, -2, 3, -4, 5, -6, 7, -8, 9];
+let positive = [];
+
+for (let i = 0; i <= arr.length; i++) {
+	if (arr[i] > 0) {
+		positive.push(arr[i]);
+	}
+
+}
+
+console.log(positive);
