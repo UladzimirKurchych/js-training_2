85a76ad605d3407e42ebb85794c706df1d31e9e3
UladzimirKurchych <kurchich.v@gmail.com>
2023-07-02 15:09:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 
-let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
-let obj2 = {};
-
-let key = [a, b, c, d, e];
-
-for (let i = 0; i < key.length -1; i++) {
-	if (obj[key[i]] % 2 === 0) {
-		obj2[key[i]] = obj[key[i]];
-	}
-
-
-}
-
-console.log(obj2);
